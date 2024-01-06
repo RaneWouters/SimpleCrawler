185560d1342065b00b91718ab971a72c5e06c996
RaneWouters <1540656951@qq.com>
2024-01-06 13:26:39 +0800

    modify the renumber func

--- renumber.py
+++ renumber.py
@@ -4,7 +4,12 @@ images_path = './images'
 images_name = os.listdir(images_path)
 images_name.sort()
 
+output_path = './images_renumbered'
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
 for i in range(len(images_name)):
-    os.rename(os.path.join(images_path, images_name[i]),
-              os.path.join(images_path,
-                           str(i).rjust(4, '0') + '.jpg'))
+    os.system('cp {} {}'.format(
+        os.path.join(images_path, images_name[i]),
+        os.path.join(output_path,
+                     str(i).rjust(4, '0') + '.jpg')))
